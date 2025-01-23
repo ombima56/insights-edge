d769b63d879b2065fb96ff2c6949391c9421ba13
John <johneliud4@gmail.com>
2025-01-23 17:01:03 +0300

    add: preliminary functionality for light/dark mode functionality.

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -123,3 +123,26 @@ document.addEventListener('DOMContentLoaded', () => {
     statsObserver.observe(stat);
   });
 });
+
+const themeToggler = document.querySelector('.theme-toggler');
+
+const applyTheme = (theme) => {
+  if (theme === 'dark') {
+    document.body.classList.add('dark-theme');
+  } else {
+    document.body.classList.remove('dark-theme');
+  }
+};
+
+const toggleTheme = () => {
+  const currentTheme = document.body.classList.contains('dark-theme')
+    ? 'dark'
+    : 'light';
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  applyTheme(newTheme);
+  localStorage.setItem('theme', newTheme);
+};
+const savedTheme = localStorage.getItem('theme') || 'light';
+applyTheme(savedTheme);
+
+themeToggler.addEventListener('click', toggleTheme);
