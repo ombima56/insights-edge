a98f1f0481d0547b443fcbcaa69da369a8400bda
John <johneliud4@gmail.com>
2025-01-23 16:19:34 +0300

    add: event listener that waits for loading of DOM elements before triggering active class to modal element.

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,1 +1,23 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById('waitlistModal');
+  const openWaitlist = document.getElementById('openWaitlist');
+  const closeModal = document.querySelector('.modal-close');
+
+  if (openWaitlist) {
+    openWaitlist.addEventListener('click', () => {
+      modal.style.display = 'flex';
+      document.body.style.overflow = 'hidden';
+      modal.classList.add('modal-active');
+    });
+  }
+
+  if (closeModal) {
+    closeModal.addEventListener('click', () => {
+      modal.classList.remove('modal-active');
+      setTimeout(() => {
+        modal.style.display = 'none';
+        document.body.style.overflow = 'auto';
+      }, 300);
+    });
+  }
+});
