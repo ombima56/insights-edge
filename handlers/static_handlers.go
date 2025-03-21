883d2bae51ca08225917173a770517cdb140fa0f
johneliud <johneliud4@gmail.com>
2025-03-21 04:42:34 +0300

    feat(handlers): add AuthHandler to serve authentication page

--- handlers/static_handlers.go
+++ handlers/static_handlers.go
@@ -15,6 +15,12 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "frontend/templates/index.html")
 }
 
+// AuthHandler serves the authentication page
+func AuthHandler(w http.ResponseWriter, r *http.Request) {
+	// Serve the auth.html file from the templates directory
+	http.ServeFile(w, r, "frontend/templates/auth.html")
+}
+
 // SetupStaticFileServer configures the static file server for CSS, JS, etc.
 func SetupStaticFileServer() {
 	fs := http.FileServer(http.Dir("frontend/static"))
