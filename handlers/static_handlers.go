fa6fd1ecf00f34911a763fea10276f5248a12194
johneliud <johneliud4@gmail.com>
2025-03-21 04:17:26 +0300

    feat(handlers): add home handler and static file server setup

---
+++ handlers/static_handlers.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+// HomeHandler serves the main page
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Serve the index.html file from the templates directory
+	http.ServeFile(w, r, "frontend/templates/index.html")
+}
+
+// SetupStaticFileServer configures the static file server for CSS, JS, etc.
+func SetupStaticFileServer() {
+	fs := http.FileServer(http.Dir("frontend/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+}
