a4614ea8bc0356cfe671d12f2a90b93f1a125f33
johneliud <johneliud4@gmail.com>
2025-03-31 18:39:09 +0300

    feat: implement database initialization, routing, and authentication middleware

--- main.go
+++ main.go
@@ -5,31 +5,46 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/ombima56/insights-edge/internal/database"
-	"github.com/ombima56/insights-edge/internal/routes"
-	"github.com/ombima56/insights-edge/internal/util"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"github.com/ombima56/insights-edge/backend/database"
+	"github.com/ombima56/insights-edge/backend/handlers"
+	"github.com/ombima56/insights-edge/backend/repository"
+	"github.com/ombima56/insights-edge/backend/routes"
+	"github.com/ombima56/insights-edge/backend/service"
 )
 
 func main() {
-	err := os.Mkdir("db", 0744)
-	if err != nil && !os.IsExist(err) {
-		log.Printf("Error: %v", err)
+	dbErr := database.InitDB()
+	if dbErr != nil {
+		log.Fatal("Failed to initialize database:", dbErr)
+	}
+	defer func() {
+		if err := database.DB.Close(); err != nil {
+			log.Println("Failed to close database connection:", err)
+		}
+	}()
+
+	store := sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+
+	userRepository := repository.NewUserRepository(database.DB)
+	insightRepository := repository.NewInsightRepository(database.DB)
+
+	userService := service.NewUserService(userRepository)
+	insightService := service.NewInsightService(insightRepository, userRepository)
+
+	authHandler := handlers.NewAuthHandler(userService, store)
+	insightHandler := handlers.NewInsightHandler(insightService)
+
+	r := mux.NewRouter()
+
+	routes.SetupRoutes(r, authHandler, insightHandler, store)
+
+	srv := &http.Server{
+		Addr:    ":9000",
+		Handler: r,
 	}
 
-	// Initialize database
-	if err := database.InitDB(); err != nil {
-		log.Fatal(err)
-	}
-
-	routes.InitRoutes()
-
-	port, err := util.ValidatePort()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("Server starting on http://localhost%v\n", port)
-	if err := http.ListenAndServe(port, nil); err != nil {
-		log.Fatal(err)
-	}
+	log.Printf("Server started on http://localhost%s\n", srv.Addr)
+	log.Fatal(srv.ListenAndServe())
 }
